be597543ca764a066f3c6a0059422241f5b858b3
chenqf <chenqinfuqq@163.com>
2019-12-03 22:21:54 +0800

    静态页面引入到Vue中

--- src/router.js
+++ src/router.js
@@ -7,6 +7,7 @@ export default new Router({
     routes : [
         {
             path : '/',
+            alias : '/cart',
             name : 'cart',
             meta : {
                 auth : true
