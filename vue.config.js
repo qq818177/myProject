8dc0369f429692746b49fe80609ead6d74f91710
chenqf <chenqinfuqq@163.com>
2019-12-03 21:15:20 +0800

    添加代理配置

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  publicPath: '/myapp'
+  publicPath: '/myapp',
+  devServer:{
+    host:'localhost',
+    port: 9080,
+    proxy:{
+      // 添加代理，当访问/mock的时候，代理到target，
+      '/mock':{
+        target:'http://127.0.0.1:5500/cart.json',
+        ws: false,
+        changeOrigin: false
+      }
+    }
+  }
 }
